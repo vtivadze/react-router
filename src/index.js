297a8dbb648267e5fe1d03fed78330a36ad844a1
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-14 14:15:28 +0400

    Define routes for elements

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
+import Invoices from './routes/invoices.jsx';
+import Expenses from './routes/expenses.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
