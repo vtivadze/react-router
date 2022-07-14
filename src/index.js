802b9e72df289f0d2924c101a0e4b6f52fb0e408
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-14 15:54:27 +0400

    Add not found route

--- src/index.js
+++ src/index.js
@@ -15,6 +15,14 @@ root.render(
         <Route path="/" element={<App />}>
           <Route path="expenses" element={<Expenses />} />
           <Route path="invoices" element={<Invoices />} />
+          <Route
+            path="*"
+            element={
+              <main style={{padding: "1rem"}}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
