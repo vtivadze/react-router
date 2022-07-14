b3fb49de94bff141f6b1101400de636cb0a590eb
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-14 17:55:09 +0400

    Swap Link by NavLink

--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,24 +1,30 @@
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import { getInvoices } from '../data';
 
 export default function Invoices() {
   let invoices = getInvoices();
   return (
-    <div style={{ display: "flex" }}>
+    <div style={{ display: 'flex' }}>
       <nav
         style={{
-          borderRight: "solid 1px",
-          padding: "1rem",
+          borderRight: 'solid 1px',
+          padding: '1rem',
         }}
       >
-        {invoices.map((invoice) => (
-          <Link
-            style={{ display: "block", margin: "1rem 0" }}
+        {invoices.map(invoice => (
+          <NavLink
+            style={({ isActive }) => {
+              return {
+                display: 'block',
+                margin: '1rem 0',
+                color: isActive ? 'red' : '',
+              };
+            }}
             to={`/invoices/${invoice.number}`}
             key={invoice.number}
           >
             {invoice.name}
-          </Link>
+          </NavLink>
         ))}
       </nav>
       {<Outlet />}
