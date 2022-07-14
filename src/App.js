9f615db1246362766520bf3ef1a3a939ef90dfa3
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-14 14:18:15 +0400

    Realize nested routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
         <Link to="/invoices">Invoices</Link> |{" "}
         <Link to="/expenses">Expenses</Link>
       </nav>
+      <Outlet />
     </div>
   );
 }
