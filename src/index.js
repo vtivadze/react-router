78af372e4669e7712e363916bb64e6d552042db6
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-14 17:51:04 +0400

    Introduce index route

--- src/index.js
+++ src/index.js
@@ -13,15 +13,23 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="expenses" element={<Expenses />} />
-          <Route path="invoices" element={<Invoices />}>
-            <Route path=":invoiceId" element={<Invoice />} />
+        <Route path='/' element={<App />}>
+          <Route path='expenses' element={<Expenses />} />
+          <Route path='invoices' element={<Invoices />}>
+            <Route
+              index
+              element={
+                <main style={{ padding: '1rem' }}>
+                  <p>Select an invoice</p>
+                </main>
+              }
+            />
+            <Route path=':invoiceId' element={<Invoice />} />
           </Route>
           <Route
-            path="*"
+            path='*'
             element={
-              <main style={{padding: "1rem"}}>
+              <main style={{ padding: '1rem' }}>
                 <p>There's nothing here!</p>
               </main>
             }
